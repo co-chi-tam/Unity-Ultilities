4e6b0dd59e4148137d872f50e690906cb9785640
Co Chi Tam <cochitam1990@gmail.com>
2017-03-23 14:09:20 +0700

    First Commit + FSM System + MiniJSON + TinyJSON + LineCollider2D

---
+++ LineCollider/CLineCollider2D.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LineCollider2D {
+	[RequireComponent(typeof (LineRenderer))]
+	[RequireComponent(typeof(EdgeCollider2D))]
+	public class CLineCollider2D : MonoBehaviour {
+
+		private LineRenderer m_LineRenderer;
+		private EdgeCollider2D m_EdgeCollider;
+		private List<Vector2> m_Points;
+
+		private void Awake() {
+			m_LineRenderer = this.GetComponent<LineRenderer> ();
+			m_EdgeCollider = this.GetComponent<EdgeCollider2D> ();
+			m_Points = new List<Vector2> ();
+		}
+
+		public void UpdateLine(Vector2 point) {
+			if (m_Points.Count == 0) {
+				SetPoint (point);
+				return;
+			}
+			if (Vector2.Distance (m_Points [m_Points.Count - 1], point) > 0.1f) {
+				SetPoint (point);
+			}
+		}
+
+		private void SetPoint(Vector2 point) {
+			m_Points.Add (point);
+			m_LineRenderer.SetVertexCount (m_Points.Count);
+			var edgePoint = new Vector2 [m_Points.Count];
+			for (int i = m_Points.Count - 1; i >= 0; i--) {
+				m_LineRenderer.SetPosition (i, m_Points[m_Points.Count - 1 - i]);
+				edgePoint [i] = m_Points [i];
+			}
+			if (m_Points.Count > 1) {
+				m_EdgeCollider.points = edgePoint;
+			}
+		}
+
+	}
+}
---
+++ LineCollider/CLineCreator2D.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LineCollider2D {
+	public class CLineCreator2D : MonoBehaviour {
+
+		[SerializeField]	private CLineCollider2D m_LinePrefab;
+		[SerializeField]	private List<CLineCollider2D> m_Lines;
+
+		private CLineCollider2D m_ActiveLine;
+
+		private void Start() {
+			m_Lines = new List<CLineCollider2D> ();
+		}
+
+		private void Update() {
+			if (Input.GetMouseButtonDown (0)) {
+				var lineGo = Instantiate (m_LinePrefab);
+				m_ActiveLine = lineGo;
+			}
+			if (Input.GetMouseButtonUp (0)) {
+				m_ActiveLine = null;
+			}
+			if (m_ActiveLine != null) {
+				var mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				m_ActiveLine.UpdateLine (mousePos);
+			}
+
+		}
+	
+	}
+}
