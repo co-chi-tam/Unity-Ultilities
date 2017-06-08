77834f636a8701ed2979dab2d61b1bcebe32d429
Co Chi Tam <cochitam1990@gmail.com>
2017-06-08 16:23:03 +0700

    + Update BuildAssetBundle

--- Assetbundle/BuildAssetBundle.cs
+++ Assetbundle/BuildAssetBundle.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using UnityEngine;
 using UnityEditor;
 
 public class CreateAssetBundles
@@ -5,6 +8,10 @@ public class CreateAssetBundles
     [MenuItem ("Assets/Build AssetBundles")]
     static void BuildAllAssetBundles ()
     {
+		var path = Application.dataPath + "/AssetBundles";
+		if (Directory.Exists (path) == false) { 
+			Directory.CreateDirectory (path);
+		}
         BuildPipeline.BuildAssetBundles ("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneOSXUniversal);
     }
 }
